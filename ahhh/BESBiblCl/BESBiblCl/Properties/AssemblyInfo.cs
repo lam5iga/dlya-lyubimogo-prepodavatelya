cef9d0dad633d300c600e39a2354190c69035770
lam5iga <97660667+lam5iga@users.noreply.github.com>
2023-02-01 17:04:38 +0300

    Add files via upload

---
+++ ahhh/BESBiblCl/BESBiblCl/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные со сборкой.
+[assembly: AssemblyTitle("BESBiblCl")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BESBiblCl")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("030befbf-65ba-4481-9899-21e216cee302")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ahhh/BESBiblCl/BESBiblCl/Room.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BESBiblCl
+{
+    public class Room
+    {
+        double roomLenght; //длина комнаты
+        double roomWidth; //ширина комнаты
+        public double RoomLenght
+        {
+            get { return roomLenght; }
+            set { roomLenght = value; }
+        }
+        public double RoomWidth
+        {
+            get { return roomWidth; }
+            set { roomWidth = value; }
+        }
+        public double RoomPerimetr()
+        {
+            return 2 * (roomWidth + roomLenght);
+        }
+        public double RoomArea()
+        {
+            return roomLenght * roomWidth;
+        }
+        //<param name="np">
+        public double PersonArea(int np)
+        {
+            return RoomArea() / np;
+        }
+        public string Info()
+        {
+            return "Комната площадью " + RoomArea() + " кв.м ";
+        }
+
+    }
+    public class LivingRoom : Room
+    {
+        int numWin;
+        public int NumWin
+        { get { return numWin; } set { numWin = value; } }
+        public string Info()
+        {
+            return "Жилая комната площадью " + RoomArea() + " кв.м " + numWin + " окнами";
+        }
+    }
+    public class Office : Room
+    {
+        int numSockets;
+        public int Numsockets
+        { get { return numSockets } set { } }
+    }
+}
